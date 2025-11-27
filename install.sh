1a191ea2c391e1c7b6974164e131043f76371fa2
k0hamed <me@khaled.id>
2025-11-27 12:38:35 +0200

    Add tmux config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/.zprofile ~/.zprofile
 ln -s ~/.dotfiles/.aerospace.toml ~/.aerospace.toml
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/sketchybar ~/.config/sketchybar
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
